45f1f5a25bb5430a77a696ca6f44dac05a80d37e
JuanWehitt <juan.cruz.wehitt@gmail.com>
2022-06-23 21:19:11 -0300

    Creacion de estructura Spring

---
+++ src/test/java/ar/utn/frbb/tup/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.utn.frbb.tup.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
